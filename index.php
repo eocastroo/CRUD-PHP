1f10465021ab4927be78075a12ff7355807e4014
Edwin Orlando Castro. <edwinorlandocastro@hotmail.com>
2025-02-05 20:39:37 -0500

    Se realiza la primera parte del menu

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+ // colocamos las rutas de  head y del footer
+ require_once("C://xampp/htdocs/CRUD-PHP/view/head/head.php");
+
+?>
+
+<?php 
+ // colocamos las rutas de  head y del footer
+ require_once("C://xampp/htdocs/CRUD-PHP/view/head/footer.php");
+
+?>
