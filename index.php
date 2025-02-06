ba099f2384d50a41d0e3d357b0a0a25d85b0e3ea
Edwin Orlando Castro. <edwinorlandocastro@hotmail.com>
2025-02-05 22:18:37 -0500

    Se enlaza los archvios a las vistas

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 
 ?>
 
+<a href="/CRUD-PHP/username"class="btn btn-primary">Agregar un usuario</a>
+
 <?php 
  // colocamos las rutas de  head y del footer
  require_once("C://xampp/htdocs/CRUD-PHP/view/head/footer.php");
--- view/head/head.php
+++ view/head/head.php
@@ -7,9 +7,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Inicio</a>
+<div class="container-fluid bg-dark p-2 mb-3">
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="/CRUD-PHP/index.php">Inicio</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -20,13 +21,16 @@
             Username
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Usuarios</a></li>
-            <li><a class="dropdown-item" href="#">Agregar nuevos usuarios</a></li>
+            <li><a class="dropdown-item" href="/CRUD-PHP/username/index.php">Usuarios</a></li>
+            <li><a class="dropdown-item" href="/CRUD-PHP/username/create.php">Agregar nuevos usuarios</a></li>
           </ul>
         </li>
       </ul>
     </div>
   </div>
-</nav>
+ </nav>
+</div>
+
+<div class="container-fluid">
 
  
